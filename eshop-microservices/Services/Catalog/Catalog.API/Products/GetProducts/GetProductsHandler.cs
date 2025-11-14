caf982035a3652c81fe50018da2069493ae400f3
praveen vamsi <praveenvamsivasamsetti1607@gmail.com>
2025-11-14 15:39:56 +0530

    Seed data and pagination implemented.

--- eshop-microservices/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
+++ eshop-microservices/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -1,17 +1,19 @@
-﻿
-using Marten.Pagination;
-
-namespace Catalog.API.Products.GetProducts
+﻿namespace Catalog.API.Products.GetProducts
 {
-    public record GetProductsQuery() : IQuery<GetProductsResult>;
+    public record GetProductsQuery(int? PageNumber = 1, int? PageSize = 10) : IQuery<GetProductsResult>;
     public record GetProductsResult(IEnumerable<Product> Products);
+
     public class GetProductsQueryHandler(IDocumentSession session) : IQueryHandler<GetProductsQuery, GetProductsResult>
     {
         public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
         {
-            //logger.LogInformation("GetProductsQueryHandler.Handle called with {@Query}", query);
+            // Ensure PageNumber and PageSize have default values if null
+            var pageNumber = query.PageNumber ?? 1;
+            var pageSize = query.PageSize ?? 10;
 
-            var products = await session.Query<Product>().ToListAsync(cancellationToken);
+            // Use Marten's ToPagedListAsync for pagination
+            var products = await session.Query<Product>()
+                .ToPagedListAsync(pageNumber, pageSize, cancellationToken);
 
             return new GetProductsResult(products);
         }
